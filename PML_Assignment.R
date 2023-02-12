d822c0c626994d52d07813b3d0d1b8b1114fce5c
Daniel Mackie <danielmackie@daniels-mbp.lan>
2023-02-12 20:46:15 +0800

    asdf

--- PML_Assignment.R
+++ PML_Assignment.R
@@ -82,9 +82,60 @@ training.optimized <- training.imputed[, (colnames(training.imputed) %in% lst)];
 modFit <- train(classe ~ ., data=training.optimized, method="rf", trControl=control);
 modFit
 
-#truncate the predictor variables that we don't need (insignificant) from our testing dataset
+#truncate the predictor variables that we don't need (insignificant) from our testing dataset,
+#then test for accuracy on the testing subset
 testing.optimized <- testing[, (colnames(testing) %in% lst)];
 combPred <- predict(modFit, testing.optimized);
 confusionMatrix(as.factor(testing.optimized$classe), combPred);
 
+
+#transform the test cases data into the format we need for prediction
+cases$year <- as.numeric(substr(cases$cvtd_timestamp, 7, 11)); 
+cases$month <- as.numeric(substr(cases$cvtd_timestamp, 4, 5)); 
+cases$day <- as.numeric(substr(cases$cvtd_timestamp, 1, 2)); 
+cases$hour <- as.numeric(substr(cases$cvtd_timestamp, 12, 13)); 
+cases$minute <- as.numeric(substr(cases$cvtd_timestamp, 15, 16));
+
+#remove raw timestamp and index columns from the test cases
+cases = cases[, !(colnames(cases) %in% c("X", "raw_timestamp_part_1", "raw_timestamp_part_2", "cvtd_timestamp"))];
+
+#cast to numeric where applicable in test cases dataset
+cases$kurtosis_roll_belt       <- as.numeric(cases$kurtosis_roll_belt);
+cases$kurtosis_picth_belt      <- as.numeric(cases$kurtosis_picth_belt);
+cases$kurtosis_yaw_belt        <- as.numeric(cases$kurtosis_yaw_belt);
+cases$skewness_roll_belt       <- as.numeric(cases$skewness_roll_belt);
+cases$skewness_roll_belt.1     <- as.numeric(cases$skewness_roll_belt.1);
+cases$skewness_yaw_belt        <- as.numeric(cases$skewness_yaw_belt);
+cases$max_yaw_belt             <- as.numeric(cases$max_yaw_belt);
+cases$min_yaw_belt1            <- as.numeric(cases$min_yaw_belt);
+cases$amplitude_yaw_belt       <- as.numeric(cases$amplitude_yaw_belt);
+cases$kurtosis_yaw_arm         <- as.numeric(cases$kurtosis_yaw_arm);
+cases$skewness_roll_arm        <- as.numeric(cases$skewness_roll_arm);
+cases$skewness_pitch_arm       <- as.numeric(cases$skewness_pitch_arm);
+cases$skewness_yaw_arm         <- as.numeric(cases$skewness_yaw_arm);
+cases$kurtosis_roll_dumbbell   <- as.numeric(cases$kurtosis_roll_dumbbell);
+cases$kurtosis_picth_dumbbell  <- as.numeric(cases$kurtosis_picth_dumbbel);
+cases$kurtosis_yaw_dumbbell    <- as.numeric(cases$kurtosis_yaw_dumbbell);
+cases$skewness_roll_dumbbell   <- as.numeric(cases$skewness_roll_dumbbell);
+cases$skewness_pitch_dumbbell  <- as.numeric(cases$skewness_pitch_dumbbell);
+cases$skewness_yaw_dumbbell    <- as.numeric(cases$skewness_yaw_dumbbell);
+cases$max_yaw_dumbbell         <- as.numeric(cases$max_yaw_dumbbell);
+cases$min_yaw_dumbbell         <- as.numeric(cases$min_yaw_dumbbell);
+cases$amplitude_yaw_dumbbell   <- as.numeric(cases$amplitude_yaw_dumbbell);
+cases$kurtosis_roll_forearm    <- as.numeric(cases$kurtosis_roll_forearm);
+cases$kurtosis_picth_forearm   <- as.numeric(cases$kurtosis_picth_forearm);
+cases$kurtosis_yaw_torearm     <- as.numeric(cases$kurtosis_yaw_forearm);
+cases$skewness_roll_forearm    <- as.numeric(cases$skewness_roll_forearm);
+cases$skewness_pitch_forearm   <- as.numeric(cases$skewness_pitch_forearm);
+cases$skewnessyawforearm       <- as.numeric(cases$skewness_yaw_forearm);
+cases$max_yaw_forearm          <- as.numeric(cases$max_yaw_forearm);
+cases$min_yaw_forearm          <- as.numeric(cases$min_yaw_forearm);
+cases$amplitude_yaw_forearm    <- as.numeric(cases$amplitude_yaw_forearm);
+cases$kurtosis_roll_arm        <- as.numeric(cases$kurtosis_roll_arm);
+cases$kurtosis_picth_arm       <- as.numeric(cases$kurtosis_picth_arm);
+
+#predict the result of the 20 test cases
+cases.optimized <- cases[, (colnames(cases) %in% c(lst, "problem_id"))];
+results <- data.frame(problem_id = cases.optimized, prediction = predict(modFit,cases.optimized));
+results;
 
